ebc84bb6ade8fa2588ed834112f8775550a02a35
Jonathan Lewis <jonathan123391@gmail.com>
2025-05-04 13:20:08 +0200

    Added debugging messages

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,12 +1,14 @@
-const admin = require("firebase-admin");
-// const serviceAccount = require("./serviceAccountKey.json");
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+   try{const admin = require("firebase-admin");
+    // const serviceAccount = require("./serviceAccountKey.json");
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://sd2025law.firebaseio.com"
-});
-
+    admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://sd2025law.firebaseio.com"
+    });} 
+catch (error) {
+    console.error("Firebase init error:", error);
+  }
 const db = admin.firestore();
 const auth = admin.auth();
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-
+require("./firebase/admin");
 
 const app = express();
 
