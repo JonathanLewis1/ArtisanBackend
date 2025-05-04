449a3199a43e0ed7e1853c34aa9658707298611c
Jonathan Lewis <jonathan123391@gmail.com>
2025-05-04 14:00:24 +0200

    Chnaged debugging

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,25 @@
+// const express = require("express");
+// const router = express.Router();
+// const AuthController = require("../controllers/authController");
+
+// router.post("/login", AuthController.login);
+// router.post("/signup", AuthController.signup);
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/authController");
 
-router.post("/login", AuthController.login);
-router.post("/signup", AuthController.signup);
+console.log("📥 Auth route file loaded");
+
+router.post("/login", (req, res, next) => {
+  console.log("🔐 Login route hit with body:", req.body);
+  next();
+}, AuthController.login);
+
+router.post("/signup", (req, res, next) => {
+  console.log("📝 Signup route hit with body:", req.body);
+  next();
+}, AuthController.signup);
 
 module.exports = router;
